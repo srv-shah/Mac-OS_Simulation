3e3df7e5cf2d25f905c4fd1db1b66aed700d0d82
Saurav Kumar <sauravkrjmu03@gmail.com>
2026-06-27 11:42:17 +0530

    upadte Loader.scss path

--- src/components/LoaderPage.jsx
+++ src/components/LoaderPage.jsx
@@ -1,28 +1,4 @@
-// import React from 'react'
-// import './LoaderPage.scss'
-
-// const LoaderPage = () => {
-//   return (
-//     <div className="loader-page">
-//       <div className="loader-card">
-//         <div className="loader-badge">macOS Portfolio</div>
-//         <h1>Starting up your portfolio...</h1>
-//         <p>Preparing the desktop experience. Please wait a moment.</p>
-//         <div className="loader-dots">
-//           <span />
-//           <span />
-//           <span />
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default LoaderPage
-
-
-
-import "./loaderPage.scss";
+import "./LoaderPage.scss";
 import { useEffect, useState } from "react";
 
 const Loader = ({ setIsLoading }) => {
